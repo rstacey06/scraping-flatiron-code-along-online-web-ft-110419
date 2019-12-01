255615d4319405c80e4a5c851edaa531e19cd0ae
Burbage Stacey <rstacey06@gmail.com>
2019-12-01 18:49:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,6 +14,7 @@ doc.css(".post").each do |post|
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
+      binding.pry
     end
  
 Scraper.new.get_page
